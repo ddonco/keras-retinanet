9027d594edb5cfdee5df7e21fa4d141158c85f5f
ddonco <dillon.donohue@gates.com>
2019-09-06 16:12:10 -0600

    feat: add argparse argument for defect transforms, make draw_captions font larger and more bold

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -243,6 +243,23 @@ def create_generators(args, preprocess_image):
             hue_range=(-0.05, 0.05),
             saturation_range=(0.95, 1.05)
         )
+    elif args.defect_transform:
+        transform_generator = random_transform_generator(
+            min_rotation=-0.1,
+            max_rotation=0.1,
+            min_translation=(-0.1, -0.1),
+            max_translation=(0.1, 0.1),
+            min_shear=-0.1,
+            max_shear=0.1,
+            flip_x_chance=0.5,
+            flip_y_chance=0.5,
+        )
+        visual_effect_generator = random_visual_effect_generator(
+            contrast_range=(0.9, 1.1),
+            brightness_range=(-.1, .1),
+            hue_range=(-0.05, 0.05),
+            saturation_range=(0.95, 1.05)
+        )
     else:
         transform_generator = random_transform_generator(flip_x_chance=0.5)
         visual_effect_generator = None
@@ -424,6 +441,7 @@ def parse_args(args):
     parser.add_argument('--no-evaluation',    help='Disable per epoch evaluation.', dest='evaluation', action='store_false')
     parser.add_argument('--freeze-backbone',  help='Freeze training of backbone layers.', action='store_true')
     parser.add_argument('--random-transform', help='Randomly transform image and annotations.', action='store_true')
+    parser.add_argument('--defect-transform', help='Apply defect detection transforms to image and annotations.', action='store_true')
     parser.add_argument('--image-min-side',   help='Rescale the image so the smallest side is min_side.', type=int, default=800)
     parser.add_argument('--image-max-side',   help='Rescale the image if the largest side is larger than max_side.', type=int, default=1333)
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file.')
